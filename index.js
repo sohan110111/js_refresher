84c9e7c6acb553336315630169161322416387a5
sohan110111 <sohan110111@gmail.com>
2024-01-06 21:50:55 +0600

    understanding ver, let, const

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var testVariable = "testName";
-function func(topic) {
-    var testVariable = "changed";
-    console.log(topic);
+// var testVariable = "testName";
+// function func(topic) {
+//     var testVariable = "changed";
+//     console.log(topic);
+// }
+
+// func("testFunc");
+// console.log(testVariable);
+
+
+
+const a = {
+    name: "test",
+    age: 20,
+    func: function() {
+        console.log(this.name);
+    }
 }
 
-func("testFunc");
-console.log(testVariable);
+console.log(a.name);
+a.name = "changed Name";
+console.log(a.name);
